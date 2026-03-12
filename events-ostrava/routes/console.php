514888b0c26aba06dc4ba83225da0c23da4e19a2
Maksym Dmytrychenko <maksymdmytrychenko@gmail.com>
2026-03-12 16:06:56 +0100

    Fixed cron jobs

--- events-ostrava/routes/console.php
+++ events-ostrava/routes/console.php
@@ -1,8 +1,45 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
+declare(strict_types=1);
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+use Illuminate\Support\Facades\Schedule;
+
+Schedule::command('events:scrape visitostrava --days=14')
+    ->twiceDaily(6, 18)
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('events:scrape allevents --days=60')
+    ->twiceDaily(7, 19)
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('events:scrape kulturajih --days=30')
+    ->twiceDaily(8, 20)
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('events:scrape kudyznudy --days=30')
+    ->twiceDaily(9, 21)
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('events:enrich --limit=15')
+    ->everyFourHours()
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('events:deactivate-past --grace-hours=2')
+    ->hourly()
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('telegram:notify')
+    ->weeklyOn(5, '08:00')
+    ->withoutOverlapping()
+    ->runInBackground();
+
+Schedule::command('telegram:notify-new')
+    ->dailyAt('10:00')
+    ->withoutOverlapping()
+    ->runInBackground();
